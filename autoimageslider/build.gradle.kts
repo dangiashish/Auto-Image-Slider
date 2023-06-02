c7dcec3d4c8dc931efe6cd151979f01da03e9ad8
Ashish Choudhary <asgishdangi96@gmail.com>
2023-06-02 18:53:26 +0530

    ashish

--- autoimageslider/build.gradle.kts
+++ autoimageslider/build.gradle.kts
@@ -59,7 +59,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.dangiashish"
             artifactId = "autoimageslider"
-            version = "1.0.0"
+            version = "1.0.1"
 
             afterEvaluate {
                 from(components["release"])
