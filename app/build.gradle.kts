8cc88e3131fadeca34ed3c7124eb251db90dd06f
Ashish <ashishdangi96@gmail.com>
2023-11-13 19:08:50 +0530

    base

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,7 @@ android {
         release {
             isMinifyEnabled = false
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
             multiDexEnabled = true
         }
@@ -61,6 +60,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.multidex:multidex:2.0.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.github.dangiashish:styledcardview:1.0.0")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(project(mapOf("path" to ":autoimageslider")))
